a20b28eff5f9cee6a63cfef0591e91180da56524
Izu-33 <dizundu@yahoo.com>
2025-01-02 04:25:57 +0100

    feat: add logic to play a round - playRound function

--- index.js
+++ index.js
@@ -15,4 +15,35 @@ function getHumanChoice() {
     return choice;
 };
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    const validChoices = ["rock", "paper", "scissors"];
+    while(!validChoices.includes(humanChoice)) {
+        console.log('Please choose "rock", "paper", or "scissors".');
+        humanChoice = getHumanChoice().toLowerCase();
+    }
+
+    if(humanChoice === computerChoice) {
+        console.log(`It's a tie! Both players chose: ${humanChoice}`);
+    } else if (
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}. 
+                    Player: ${humanScore} vs Computer: ${computerScore}`);
+    } else {
+        computerScore++;
+        console.log(`You lose. ${humanChoice} loses to ${computerChoice}. 
+                    Player: ${humanScore} vs Computer: ${computerScore}`);
+    }
+};
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
