99cd13e461a0f1dd35e135c44c9559a16b183dd3
Izu-33 <dizundu@yahoo.com>
2025-01-02 03:25:07 +0100

    feat: add function to get computer choice

---
+++ index.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    const randomNumber = Math.random();
+
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+};
+
+console.log(getComputerChoice());
